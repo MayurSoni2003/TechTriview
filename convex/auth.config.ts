1d6b55f69c019727c766d45a49b0d2d5f2761390
Mayur Soni <mayur2003soni@gmail.com>
2025-03-09 00:24:28 +0530

    In running state

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://climbing-cattle-3.clerk.accounts.dev/",
+      domain: "https://related-redfish-21.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
